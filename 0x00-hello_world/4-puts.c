f1e025a11d44f87d81ab05ebc8fe37f81bcfc97a
Your Name <you@example.com>
2023-03-17 09:52:02 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main . A program that uses the put and print function to print a line
+ * Return: 0(success)
+ */
+int main(void)
+{
+     puts("Programming is like building a multilingual puzzle\n");
+
+     return (0);
+}
+
