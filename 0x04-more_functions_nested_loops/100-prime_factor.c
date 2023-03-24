929b9cb33e530edc750e5461de9c9e9f28b1e7c5
Your Name <you@example.com>
2023-03-24 08:12:19 -0700

    nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -9,28 +9,17 @@
  */
 int main(void)
 {
-	long int n;
-	long int man;
-	long int i;
+	long x, maxf;
+	long number = 612852475143;
+	double square = sqrt(number);
 
-	n = 612852475143;
-	max = -1; 
-
-	while (n % 2 == 0)
-	{ 
-		max = 2;
-		n /= 2;
-	}
-	for (i = 3; i <= sqrt(n); i = i + 2)
-	{ 
-		while ( n % i == 0)
-		{ 
-			max = i;
-			n = n / i;
+	for (x = 1; x <= square; x++)
+	{
+		if (number % x == 0)
+		{
+			maxf = number / x;
 		}
 	}
-	if (n > 2)
-		max = n;
 	printf("%ld\n", max);
 	return (0);
 }
