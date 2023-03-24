4bcd012085ea9fa3429b6bb9a1840c7982b03321
Your Name <you@example.com>
2023-03-24 07:13:30 -0700

    nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,7 +10,13 @@ void print_most_numbers(void)
 	for (c = '0'; c <= '9'; c++)
 	{
 		if (|(c == '2' || c == '4'))
+		{
+			continue;
+		}
+		else
+		{
 			_putchar(c);
+		}
 	}
 	_putchar('\n');
 }
