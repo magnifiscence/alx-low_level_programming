abb26ff7778689e4c737308c6cc3587791cffcef
Your Name <you@example.com>
2023-03-24 07:59:19 -0700

    nested loop

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,24 +2,25 @@
 /**
  * 10-print_triangle.c - prints a triangle followed by a new line
  * @size: size of the triangle
+ * Return: empty
  */
 void print_trangle(int size)
 {
+	int i, j, k;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int i, j;
-
 		for (i = 1; i <= size; i++)
 		{
-			for (j = i; < size; j++)
+			for (j =size - i; j > 1; j--)
 			{
 				_putchar(' ');
 			}
-			for (j = 1; j <= i; j++)
+			for (k = 1; k <= i; k++)
 			{
 				_putchar('0');
 			}
