719a7b16e6a032e9ad8701079ce6580f10d72ee8
Your Name <you@example.com>
2023-03-30 20:01:15 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 /**
- * 101-print_number.c - prints an interger
- * @n: interger to be printed
+ * print_number - prints an integer.
+ * @n: The integer to print.
+ * Return: Nothing!
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int k = n;
 
 	if (n < 0)
 	{
-		n1 = -n;
+		n *= -1;
+		k = n;
 		_putchar('-');
-	} else
-	{
-		nl = n;
 	}
-	if (nl / 10)
-	{
-		print_number(n1 / 10);
-	}
-	_putchar((n1 % 10) * '0');
+	k /= 10;
+	if (k != 0)
+		print_number(k);
+	_putchar((unsigned int) n % 10 + '0');
 }
