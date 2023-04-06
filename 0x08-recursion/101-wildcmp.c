9e4ea9dfbbf45385030627880bccc9e188e4641c
Your Name <you@example.com>
2023-04-05 19:00:13 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * wildcmp _ compare strings and returns 1 if strings are
+ * identical and 0 if they are not.
+ * @s1: pointer to string parameters
+ * @s2: pointer to string parameters
+ * Return: 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+				}
+				else if (*s1 == *s2)
+				{
+				return (wildcmp(s1 + 1, s2 + 1));
+				}
+				return (0);
+				}
