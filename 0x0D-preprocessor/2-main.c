991907ff828093001c9b758316c29397fd981c98
Your Name <you@example.com>
2023-04-18 07:40:00 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,15 @@
+#ifndef _FILE_H
+#define _FILE_H
 #include <stdio.h>
 /**
- * main - prints file name
+ * main - Entry point
+ * Description: prints name of file from which
+ * it was compiled.
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
+#endif
