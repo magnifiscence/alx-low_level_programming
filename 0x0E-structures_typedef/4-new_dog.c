46b54837b8e0ae4e1b0e1f3607d8fb091537254f
Your Name <you@example.com>
2023-04-18 07:58:16 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -60,15 +60,22 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
-	return (NULL);
-dog->name = malloc(sizeof(char) * (len2 + 1));
-if (dog->owner == NULL)
-{
-	free(dog);
-	return (NULL);
-}
-_strcpy(dog->name, name);
-_strcpy(dog->owner, owner);
-dog->age = age;
-return (dog);
+		return (NULL);
+
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
+		return (NULL);
+	}
+	_strcpy(dog->name, name);
+	_strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
 }
