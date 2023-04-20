d7d69450ad27a05aee7dc8ce7e71ca50063dd595
Your Name <you@example.com>
2023-04-20 09:55:04 -0700

    1-print_numbers.c

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,37 @@
+#ifndef SEPERATOR_H
+#define SEPERATOR_H
+
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - function prints strings followed by a newline
+ * @seperator: string to be printed between strings
+ * @n: number of arguments passed to function
+ * @...: ellipse
+ * description: if seperator is NULL it is not printed
+ * if one of the string is NULL (nill) is printed
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	char *str;
+	unsigned int index;
+
+	va_start(strings, n);
+
+	for (index = 0; index < n; index++)
+	{
+		str = va_arg(strings, char *);
+
+		if (str == NULL)
+			printf("nill)");
+		else
+			printf("%s", str);
+		if (index != (n - 1) && seperator != NULL)
+			printf("%s", seperator);
+	}
+	printf("\n");
+	va_end(strings);
+}
+#endif
