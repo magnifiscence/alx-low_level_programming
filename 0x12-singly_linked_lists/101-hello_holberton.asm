742873d0243eb27652b74f0e3876b8bfd4336fd8
Your Name <you@example.com>
2023-04-27 20:53:25 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,11 @@
-global main
+global _start
+
+section .text
 extern printf
-main:
-mov edi, format
+_start:
+mov edi, msg
 xor eax, eax
 call printf
 mov eax, 0
 ret
-format: db `Hello, Holberton\n`,0
+msg: db `Hello, Holberton\n`,0
