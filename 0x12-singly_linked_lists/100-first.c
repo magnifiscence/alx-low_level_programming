e09b4d6d4eb5287768ccb6d898d6aa6654341f3e
Your Name <you@example.com>
2023-04-27 19:42:53 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void first(void) __attribute__ ((constructor));
+/**
+ * first - function prints
+ * You're beat! and yet, you must allow,\nI bore my house upon my back!\n
+ * before main is executed
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow, \n");
+	printf("i bore my house upon my back!\n");
+}
