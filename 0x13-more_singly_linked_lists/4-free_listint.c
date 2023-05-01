6f9259e9de21365710fd1eb8821cbe3e81d6cc58
Your Name <you@example.com>
2023-05-01 06:46:38 -0700

    4-free_listint.c

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - function that frees a list
+ * @head: pointer to the list listint_t which is to be freed.
+ */
+void free_listint(listint_t *head)
+{
+	/*lets declare a temporal pointer to a list*/
+	listint_t *temp;
+
+	while (head)
+	{
+		head->next = temp;
+		free(head);
+		head = temp;
+	}
+}
