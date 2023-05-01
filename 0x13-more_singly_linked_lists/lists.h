fb3cb5f65499d7f87a9dc8eb876ac8401e7d68c8
Your Name <you@example.com>
2023-04-30 21:06:45 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -30,4 +30,5 @@ typedef struct listint_s
 	size_t print_listint_safe(const listint_t *head);
 	size_t free_listint_safe(listint_t **h);
 	listint_t *find_listint_loop(listint_t *head);
+	int _putchar(char c);
 #endif
