cf35d8483ee508f8e273dc6e187c68d199726f86
Your Name <you@example.com>
2023-05-08 10:21:53 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - function that creates a file.
+ * @filename: pointer to filename being created.
+ * @text_content: pointer to content written in file.
+ * Return: 1 on success.
+ * -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int w, fd, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+	close(fd);
+
+	return (1);
+}
