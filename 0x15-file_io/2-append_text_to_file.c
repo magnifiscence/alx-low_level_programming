9c7179d244e6281e82c8aeb6df3967f1699466fd
magnifiscence <www.divinedivinepraise@gmail.com>
2023-07-10 18:22:29 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,18 @@
 #include "main.h"
 /**
+ * _strlen - finds string length
+ * @str: string
+ * Return: length
+ */
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0';)
+	len++;
+	return (len);
+
+/**
  * append_text_to_file - function appends text at the end of file.
  * @filename: pointer
  * @text_content: string to add to end of file
@@ -8,18 +21,24 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int w, o, len = 0;
+	int fd, n_wrote;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	if (text_content != NULL)
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		close(fd);
+		return (1);
 	}
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
-	return (-1);
-	close(0);
+	n_wrote = write(fd, text_content, _strlen(text_content));
+	if (n_wrote == -1 || n_wrote != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
 	return (1);
 }
